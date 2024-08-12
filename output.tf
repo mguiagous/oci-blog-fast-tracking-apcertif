db8bca103c50f60d365a3056f1e82d10973abc38
Mahamat Guiagoussou <mahamat.guiagoussou@oracle.com>
2024-08-11 22:11:09 -0700

    Update output.tf
    Added comments

--- output.tf
+++ output.tf
@@ -1,26 +1,34 @@
+# Working Compartment OCID
 output "compartment_id" {
   value = var.compartment_id
 }
 
+# Instance Image OCID
 output "instance_image_id" {
   value = var.amper_image_id
 }
 
+# Display Name Prefix
 output "display_name_prefix" {
   value = var.display_name_prefix
 }
 
-
+# VCN OCID
 output "public_vcn_id" {
   value = var.vcn_id
 }
+
+# Public Subnet OCID
 output "public_subnet_id" {
   value = var.public_subnet_id
 }
+
+# Private Subnet OCID
 output "private_subnet_id" {
   value = var.private_subnet_id
 }
 
+# Public VM OCID, Display Name & Public IP
 output "_vm1-3_id" {
   value = oci_core_instance.VM1-3.*.id
 }
@@ -35,6 +43,7 @@ output "_vm1-3_private_ip" {
 }
 
 
+# Private VM OCID, Display Name & Public IP
 output "_vm4_id" {
   value = oci_core_instance.vm-4.*.id
 }
@@ -49,7 +58,7 @@ output "_vm4_private_ip" {
 }
 
 
-
+# Network Security Groups OCID & Name 
 output "_nsg1_id" {
   value = oci_core_network_security_group.nsg-01.*.id
 }
