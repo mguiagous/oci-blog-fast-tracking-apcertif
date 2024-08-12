583342d77b1d67b8d25b88d7f9a6c8ee88eaebba
Mahamat Guiagoussou <mahamat.guiagoussou@oracle.com>
2024-08-11 22:06:13 -0700

    Update resources_clean_up.sh
    Cleaned proprietary path

--- resources_clean_up.sh
+++ resources_clean_up.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-source /Users/mguiagou/terraform-ws/lab4-demo/env-vars 
-/usr/local/bin/terraform init
-/usr/local/bin/terraform destroy --auto-approve
-/usr/local/bin/terraform destroy --auto-approve
-
+terraform init
+terraform destroy --auto-approve
